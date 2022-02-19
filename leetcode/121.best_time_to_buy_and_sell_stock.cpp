772388edfee4d6491702339e9aac1849e55ff30d
Eric <ejpham1999@gmail.com>
2022-02-19 01:45:43 -0600

    121 - O(n) solution

--- leetcode/121.best_time_to_buy_and_sell_stock.cpp
+++ leetcode/121.best_time_to_buy_and_sell_stock.cpp
@@ -1,13 +1,20 @@
 #include <vector>
+#include <climits>
 class Solution {
 public:
     int maxProfit(std::vector<int>& prices) {
-        int profit = 0;
+        // low: lowest prices to buy at
+        // high : highest profit so far
+        // profit: profit if sold today
+        int low = INT_MAX, high = 0, profit = 0;
         for (int i = 0; i < prices.size(); i++) {
-            for (int j = i + 1; j < prices.size(); j++) {
-                if (prices[j] - prices[i] > profit) profit = prices[j] - prices[i];
-            }
+            // if found lower buy price
+            if (prices[i] < low) low = prices[i];
+            // calculate profit if sold today with lowest price so far
+            profit = prices[i] - low;
+            // if profit if sold today is higher than highest profit so far
+            if (profit > high) high = profit;
         }
-        return profit;
+        return high;
     }
 };
