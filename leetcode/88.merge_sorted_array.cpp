ffc63616c5239578b7955948b07db6ee42de3d0c
Eric <ejpham1999@gmail.com>
2022-02-19 01:01:19 -0600

    88, O(m+n) solution

--- leetcode/88.merge_sorted_array.cpp
+++ leetcode/88.merge_sorted_array.cpp
@@ -1,9 +1,11 @@
 #include <vector>
-#include <algorithm>
 class Solution {
 public:
     void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
-        for (int i = m; i < m + n; i++) nums1[i] = nums2[i - m];
-        std::sort(nums1.begin(), nums1.end());
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
+        }
     }
 };
