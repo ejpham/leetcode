159e8aff12d1040f619c55e494b92a55484b28d9
Eric <ejpham1999@gmail.com>
2022-02-19 02:19:31 -0600

    1672 - O(n^2) using STL

---
+++ leetcode/1672.richest_customer_wealth.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <numeric> // std::accumulate
+class Solution {
+public:
+    int maximumWealth(std::vector<std::vector<int>>& accounts) {
+        // O(n^2) time complexity - brute force using accumulate
+        int richest = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            sum = std::accumulate(accounts[i].begin(), accounts[i].end(), sum);
+            if (sum > richest) richest = sum;
+        }
+        return richest;
+
+        /* O(n^2) time complexity - brute force solution
+        int richest = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++) {
+                sum += accounts[i][j];
+            }
+            if (sum > richest) richest = sum;
+        }
+        return richest;
+        */
+    }
+};
