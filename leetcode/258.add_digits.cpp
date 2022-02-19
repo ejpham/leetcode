a3125d9c048d4f175b7d54090288c1573c5818f0
Eric <ejpham1999@gmail.com>
2022-02-19 02:04:33 -0600

    258 - showcasing O(1) solutions

--- leetcode/258.add_digits.cpp
+++ leetcode/258.add_digits.cpp
@@ -3,5 +3,15 @@ public:
     int addDigits(int num) {
         while (num >= 10) num = (num % 10) + (num / 10);
         return num;
+        /*
+        O(1) time complexity using digit root concept
+            if (num == 0) return 0;
+            else if (num % 9 == 0) return 9;
+            else return num % 9;
+        */
+       /*
+        O(!) time complexity one liner
+            return 1 + (num - 1) % 9;
+       */
     }
 };
