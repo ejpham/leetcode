72a11f97f6d039e1ded35a3a6e9e8212a682febf
Eric-Mac <ejpham1999@gmail.com>
2022-02-20 03:16:09 -0600

    35 - more binary search zzz

---
+++ leetcode/35.search_insert_position.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+class Solution {
+public:
+    int searchInsert(std::vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1,ans;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(target==nums[mid]){
+                ans=mid;
+                break;
+            }
+            else if(left==right && nums[mid]!=target){
+                if(target>nums[mid]){
+                    ans=mid+1;
+                    break;
+                }
+                else{
+                    ans=mid;
+                    break;
+                }
+            }
+            else if(target>nums[mid]) left=mid+1;
+            else if(target<nums[mid]) right=mid-1;
+        }
+        return ans;
+    }
+};
