ff436dd6748997b36c50fe5c98bf449cd448b385
Eric-Mac <ejpham1999@gmail.com>
2022-02-21 15:01:44 -0600

    cpp:169 - added 2 other solutions

--- cpp/169.majority_element.cpp
+++ cpp/169.majority_element.cpp
@@ -1,8 +1,29 @@
+#include <algorithm>
 #include <unordered_map>
 #include <vector>
 class Solution {
 public:
     int majorityElement(std::vector<int>& nums) {
+        // O(nlogn) time, O(1) space
+        std::sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
+        
+        /* O(n) time, O(1) space
+        int ans = nums[0], cnt = 1;
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == ans) cnt++;
+            else cnt--;
+            if (cnt == 0) {
+                ans = nums[i];
+                cnt = 1;
+            }
+        }
+        
+        return ans;
+        */
+        
+        /* O(n) time, O(n) space
         std::unordered_map<int, int> ump;
         for (int i = 0; i < nums.size(); i++) {
             ump[nums[i]]++;
@@ -16,5 +37,6 @@ public:
             }
         }
         return ans;
+        */
     }
 };
