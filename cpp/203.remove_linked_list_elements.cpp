4d22efbffd8fa9e3af80f60335ed63df0f612288
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-23 01:21:12 -0600

    cpp:203 - first solution

--- cpp/203.remove_linked_list_elements.cpp
+++ cpp/203.remove_linked_list_elements.cpp
@@ -6,6 +6,7 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
