38fb973387d62e183a9bf974e271a236095a823b
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-22 20:19:57 -0600

    cpp:171 - added second solution

--- cpp/171.excel_sheet_column_number.cpp
+++ cpp/171.excel_sheet_column_number.cpp
@@ -3,12 +3,17 @@
 class Solution {
 public:
     int titleToNumber(std::string columnTitle) {
+        // three liner solution - same time and space
         int ans = 0;
-        int multi = 1;
+        for (char c : columnTitle) ans = (ans * 26) + (c - 64);
+        return ans;
+        /* first solution - O(n) time, O(1) space
+        int ans = 0, multi = 1;
         for (int i = columnTitle.length() - 1; i >= 0; i--) {
             ans += (columnTitle[i] - 64) * multi;
             if (i > 0) multi *= 26;
         }
         return ans;
+        */
     }
 };
