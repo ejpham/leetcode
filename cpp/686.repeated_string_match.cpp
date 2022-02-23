3fb3c2517413e615cb8f90c9ed1dcb8ad27d35e2
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-23 14:02:40 -0600

    cpp:686 - solution

--- cpp/686.repeated_string_match.cpp
+++ cpp/686.repeated_string_match.cpp
@@ -2,10 +2,10 @@
 class Solution {
 public:
     int repeatedStringMatch(std::string a, std::string b) {
-        if (a.find(b) != -1) return 1;
-        if (b.find(a) == -1) return -1;
-        int cnt = 1;
-        while (a.find(b) == -1) cnt++, a += a;
-        return cnt;
+        string temp = a;
+        for (int i = 1; i <= b.length() / a.length() + 2; i++, temp += a) {
+            if (temp.find(b) != std::string::npos) return i;
+        }
+        return -1;
     }
 };
