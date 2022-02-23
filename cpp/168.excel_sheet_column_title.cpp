170f1a72dbd6f26b832f4910f5380b97ddde59e9
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-22 22:49:53 -0600

    cpp:168 - unfinished

---
+++ cpp/168.excel_sheet_column_title.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <algorithm>
+class Solution {
+public:
+    std::string convertToTitle(int columnNumber) {
+        std::string ans = "";
+        /*
+        1 = "A"
+        27 = "AA"
+        52 = "AZ"
+        53 = "BA"
+        701 = "ZY"
+        702 = "ZZ"
+        703 = "AAA"
+        18279 = "AAAA"
+        */
+       
+        return ans;
+    }
+};
