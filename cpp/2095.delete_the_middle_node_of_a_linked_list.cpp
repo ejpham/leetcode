54892519d278d2bee08c4baa3ba9ca8a8c09cc34
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-23 00:55:57 -0600

    cpp:2095 - faster solution

--- cpp/2095.delete_the_middle_node_of_a_linked_list.cpp
+++ cpp/2095.delete_the_middle_node_of_a_linked_list.cpp
@@ -10,7 +10,17 @@ struct ListNode {
 class Solution {
 public: // this does NOT deallocate "deleted" node due to leetcode
     ListNode* deleteMiddle(ListNode* head) {
-        if (!head->next) return head->next;
+        if (!head->next) return nullptr;
+        ListNode *fast = head, *slow = head, *prev = slow;
+        // this will always result in slow ending in the middle of the list as fast will end at end of list or end->next
+        while (fast && fast->next) fast = fast->next->next, prev = slow, slow = slow->next;
+        prev->next = slow->next;
+        delete slow;
+        slow = nullptr;
+        return head;
+        
+        /* first solution
+        if (!head->next) return nullptr;
         ListNode *curr = head;
         int n = 0;
         while (curr) n++, curr = curr->next;
@@ -18,5 +28,6 @@ public: // this does NOT deallocate "deleted" node due to leetcode
         for (int i = 1; i < n / 2; i++) curr = curr->next;
         curr->next = curr->next->next;
         return head;
+        */
     }
 };
