bdc6fea62b531150c3bf658e8ba55acba629b52a
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-23 01:31:21 -0600

    cpp:203 - added explanation

--- cpp/203.remove_linked_list_elements.cpp
+++ cpp/203.remove_linked_list_elements.cpp
@@ -10,18 +10,18 @@ struct ListNode {
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if (!head) return nullptr;
+        if (!head) return nullptr; /* if the list is empty, return nullptr */
         ListNode *curr = head;
-        while (curr->next) {
-            if (curr->next->val == val) {
+        while (curr->next) { /* loop as the next pointer is not nullptr */
+            if (curr->next->val == val) { /* if next pointer->val == val */
                 ListNode *temp = curr->next;
-                curr->next = temp->next;
-                delete temp;
-                temp = nullptr;
+                curr->next = temp->next; /* move current->next to current->next->next to skip over deleting node */
+                delete temp; /* delete node */
+                temp = nullptr; /* deallocate node */
             }
-            else curr = curr->next;
+            else curr = curr->next; /* current only moves onto next if next->val != val */
         }
-        if (head->val == val) {
+        if (head->val == val) { /* since we did not check if head->val == val */
             ListNode *temp = head;
             head = head->next;
             delete temp;
