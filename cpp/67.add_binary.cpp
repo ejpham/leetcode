a13623966eaef8b1922455731da8ce6183793836
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-25 15:24:35 -0600

    cpp:67 - solution

--- cpp/67.add_binary.cpp
+++ cpp/67.add_binary.cpp
@@ -8,43 +8,48 @@ public:
         while (i >= 0 && j >= 0) {
             if (a[i] == '1' && b[j] == '1') {
                 if (carry) ret = "1" + ret;
-                else {
-                    ret = "0" + ret;
-                    carry = true;
-                }
+                else ret = "0" + ret;
+                carry = true;
             }
             else if (a[i] == '1' || b[j] == '1') {
                 if (carry) ret = "0" + ret;
+                else ret = "1" + ret;
+            }
+            else {
+                if (carry) {
+                    ret = "1" + ret;
+                    carry = false;
+                }
+                else ret = "0" + ret;
+            }
+            i--, j--;
+        }
+        while (i >= 0) {
+            if (carry) {
+                if (a[i] == '1') ret = "0" + ret;
                 else {
                     ret = "1" + ret;
                     carry = false;
                 }
             }
-            else {
-                ret = "0" + ret;
-                carry = false;
-            }
-            i--, j--;
-        }
-        while (i >= 0) {
-            if (carry && a[i] == '1') ret = "0" + ret;
-            else if (carry && a[i] != '1') {
-                ret = "1" + ret;
-                carry = false;
-            }
             else ret = a[i] + ret;
             i--;
         }
-        while (i >= 0) {
-            if (carry && b[j] == '1') ret = "0" + ret;
-            else if (carry && b[j] != '1') {
-                ret = "1" + ret;
-                carry = false;
+        while (j >= 0) {
+            if (carry) {
+                if (b[j] == '1') ret = "0" + ret;
+                else {
+                    ret = "1" + ret;
+                    carry = false;
+                }
             }
             else ret = b[j] + ret;
             j--;
         }
-        if (carry) ret = "1" + ret;
+        if (carry) {
+            ret = "1" + ret;
+            carry = false;
+        }
         return ret;
     }
 };
