4d33bd27be7ca28be1fde84b1f20b5db0df8f778
Eric Joseph Pham <ejpham1999@gmail.com>
2022-02-24 21:20:26 -0600

    cpp:66 - solution O(n) time O(n) space?

--- cpp/66.plus_one.cpp
+++ cpp/66.plus_one.cpp
@@ -3,15 +3,16 @@
 class Solution {
 public:
     std::vector<int> plusOne(std::vector<int>& digits) {
-        if (digits[digits.size() - 1] != 9) {
-            digits[digits.size() - 1]++;
+        int i = digits.size() - 1;
+        if (digits[i] != 9) {
+            digits[i]++;
             return digits;
         }
         std::vector<int> ans;
-        int i = digits.size() - 1;
-        while (digits[i] == 9) ans.push_back(0), i--;
+        while (i >= 0 && digits[i] == 9) ans.push_back(0), i--;
         if (i < 0) ans.push_back(1);
         else digits[i]++;
+        while (i >= 0) ans.push_back(digits[i--]);
         std::reverse(ans.begin(), ans.end());
         return ans;
     }
