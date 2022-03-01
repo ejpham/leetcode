e39682d3d7ef42d475f490558ca0805c9d4d42de
ejpham <ejpham1999@gmail.com>
2022-03-01 16:22:47 -0600

    cpp:82 - solution

--- cpp/82.remove_duplicates_from_sorted_list_ii.cpp
+++ cpp/82.remove_duplicates_from_sorted_list_ii.cpp
@@ -1,4 +1,3 @@
-#include <vector>
 // Definition for singly-linked list.
 struct ListNode {
     int val;
@@ -13,37 +12,39 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         if (!head) return nullptr;
         if (!head->next) return head;
-        ListNode *slow = head, *fast = head;
-        fast = fast->next;
-        bool firstDupe = slow->val == fast->val;
-        if (firstDupe) {
-            while (slow->val == fast->val) {
-                ListNode *temp = fast;
+        ListNode *slow = head, *fast = head->next;
+        while (fast && slow->val == fast->val) { // first two nodes are duplicates
+            int val = fast->val;
+            while (fast && fast->val == val) {
+                ListNode *ftemp = fast;
                 fast = fast->next;
-                slow->next = fast;
-                delete temp;
-                temp = nullptr;
+                delete ftemp;
+                ftemp = nullptr;
             }
-            if (!fast) {
-                delete slow;
-                slow = nullptr;
-                head = slow;
-                return head;
-            }
-            head = fast;
             delete slow;
+            if (!fast) {
+                slow = nullptr;
+                return slow;
+            }
             slow = fast;
+            head = slow;
             fast = fast->next;
         }
         while (fast && fast->next) {
-            if (slow->val == fast->val) {
-                ListNode *temp = fast;
-                fast = fast->next;
+            if (fast->val == fast->next->val) {
+                int val = fast->val;
+                while (fast && fast->val == val) {
+                    ListNode *ftemp = fast;
+                    fast = fast->next;
+                    delete ftemp;
+                    ftemp = nullptr;
+                }
                 slow->next = fast;
-                delete temp;
-                temp = nullptr;
             }
-            else slow = slow->next, fast = fast->next;
+            else {
+                slow = slow->next;
+                fast = fast->next;
+            }
         }
         return head;
     }
