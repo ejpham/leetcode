b788b53bc54ffa0399c6f08c1532644041ba9d03
ejpham <ejpham1999@gmail.com>
2022-03-01 17:10:07 -0600

    cpp:3 - removed base cases

--- cpp/3.longest_substring_without_repeating_characters.cpp
+++ cpp/3.longest_substring_without_repeating_characters.cpp
@@ -2,8 +2,6 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(std::string s) {
-        if (s.length() == 0) return 0;
-        if (s.length() == 1) return 1;
         int ans = 0;
         for (int i = 0; i < s.length(); i++) {
             std::string ss = "";
