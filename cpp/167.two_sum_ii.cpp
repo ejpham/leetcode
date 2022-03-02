9c894291ed36cc8ce604bf45b201d8575fa00e4a
ejpham <ejpham1999@gmail.com>
2022-03-02 17:19:42 -0600

    cpp:167 - unfinished

---
+++ cpp/167.two_sum_ii.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <vector>
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& numbers, int target) {
+        std::vector<int> ans;
+        
+        return {-1, -1};
+    }
+};
