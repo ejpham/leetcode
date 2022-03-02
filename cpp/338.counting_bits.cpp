57d4f7d67b0a9eff11d6cc8fc89075761a696244
ejpham <ejpham1999@gmail.com>
2022-03-01 22:23:15 -0600

    cpp:338 - optimized first solution

--- cpp/338.counting_bits.cpp
+++ cpp/338.counting_bits.cpp
@@ -5,13 +5,15 @@ public:
     std::vector<int> countBits(int n) {
         std::vector<int> ans;
         for (int i = 0; i <= n; i++) {
-            ans.push_back(0);
-            int j = i;
-            while (j > 0) {
-                if (j % 2 == 1) ans[i]++;
-                j /= 2;
-            }
+            
         }
         return ans;
+        /*
+        std::vector<int> ans(n + 1, 0);
+        for (int i = 0; i < ans.size(); i++) {
+            for (int j = i; j > 0; j /= 2) if (j % 2 == 1) ans[i]++;
+        }
+        return ans;
+        */
     }
 };
