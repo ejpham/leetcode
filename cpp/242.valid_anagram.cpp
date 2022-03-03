8111b0ef692137a7f02f595d39fac01e78311fc6
ejpham <ejpham1999@gmail.com>
2022-03-03 16:35:07 -0600

    cpp:242 - new solution, removed one for loop

--- cpp/242.valid_anagram.cpp
+++ cpp/242.valid_anagram.cpp
@@ -5,8 +5,7 @@ public:
     bool isAnagram(std::string s, std::string t) {
         if (s.length() != t.length()) return false;
         std::unordered_map<char, int> um;
-        for (int i = 0; i < s.length(); i++) um[s[i]]++;
-        for (int i = 0; i < t.length(); i++) um[t[i]]--;
+        for (int i = 0; i < s.length(); i++) um[s[i]]++, um[t[i]]--;
         for (auto x : um) if (x.second > 0) return false;
         return true;
     }
