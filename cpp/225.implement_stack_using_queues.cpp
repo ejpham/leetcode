e0bea81baf9c05e4b09b32de5d1fe0474a19ece3
ejpham <ejpham1999@gmail.com>
2022-03-03 16:13:47 -0600

    cpp:225 - unfinished

---
+++ cpp/225.implement_stack_using_queues.cpp
@@ -0,0 +1,29 @@
+#include <queue>
+class MyStack {
+public:
+    std::queue<int> que;
+    MyStack() {}
+    
+    void push(int x) {
+        
+    }
+    
+    int pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    bool empty() { return que.empty(); }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
