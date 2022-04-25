292eb1261547a8a73baa7f28685cb9671e8e476f
ejpham <ejpham1999@gmail.com>
2022-04-25 00:32:43 -0500

    js:1 two solutions

--- js/1.two_sum.js
+++ js/1.two_sum.js
@@ -4,5 +4,22 @@
  * @return {number[]}
  */
  var twoSum = function(nums, target) {
-    
+     /*
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i]))
+            return [map.get(target - nums[i]), i];
+        else
+            map.set(nums[i], i);
+    }
+	return [];
+    */
+   let map = {};
+   for (let i = 0; i < nums.length; i++) {
+       if (map[target - nums[i]] !== undefined)
+           return [map[target - nums[i]], i];
+       else
+           map[nums[i]] = i;
+   }
+   return [];
 };
